e11e9dcb626600ad2797801ee852163101fc58e2
via <113538313+via3737@users.noreply.github.com>
2023-12-14 14:39:08 +0530

    Add files via upload

---
+++ Weekday.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+
+public class Weekday
+{
+	public static void main(String args[])
+	{
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		int num = reader.nextInt();
+
+		int rem=num%7;
+
+		if(rem==0)
+		{
+			System.out.print("It is sunday !");
+		}
+		if(rem==1)
+		{
+			System.out.print("It is sunday !");
+		}
+		if(rem==2)
+		{
+			System.out.print("It is sunday !");
+		}
+		if(rem==3)
+		{
+			System.out.print("It is sunday !");
+		}
+		if(rem==4)
+		{
+			System.out.print("It is sunday !");
+		}
+		if(rem==5)
+		{
+			System.out.print("It is sunday !");
+		}
+		if(rem==6)
+		{
+			System.out.print("It is sunday !");
+		}
+	}
+}
